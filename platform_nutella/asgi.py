1e7284d4c6a74ef4ddd7a2395a6309e33e278ae6
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-04-10 12:55:57 +0200

    fichier mis a jour

--- platform_nutella/asgi.py
+++ platform_nutella/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for platform_nutella project.
+ASGI config for platform_nutellaf project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'platform_nutella.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'platform_nutellaf.settings')
 
 application = get_asgi_application()
