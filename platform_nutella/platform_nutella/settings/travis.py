7c37e88348d55616689ee2addde26beb160dc8fc
maximilien <maximilien.lehoux@gmail.com>
2021-04-17 17:54:06 +0000

    travis.py

--- platform_nutella/platform_nutella/settings/travis.py
+++ platform_nutella/platform_nutella/settings/travis.py
@@ -5,11 +5,11 @@ SECRET_KEY = 's)@1Z*\x0cF\rAHQ}2\n$VQzX95[d'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': 'nutella', # le nom de notre base de données créée précédemment
+        'USER': 'maximilien', # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': 'Veronicamars2991',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
