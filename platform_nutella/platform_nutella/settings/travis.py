38b1aa6187d426b4abd30f5a61475a999a9710a3
maximilien <maximilien.lehoux@gmail.com>
2021-04-17 16:10:50 +0000

    travis

--- platform_nutella/platform_nutella/settings/travis.py
+++ platform_nutella/platform_nutella/settings/travis.py
@@ -1,10 +1,5 @@
 from . import *
 
-SECRET_KEY = 's)@1Z*\x0cF\rAHQ}2\n$VQzX76[d'
-DEBUG = False
-ALLOWED_HOSTS = ['188.166.164.76']
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
