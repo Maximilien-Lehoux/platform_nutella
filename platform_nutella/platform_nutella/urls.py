9923f859e27777bdd91508e84c338b79ce5d38fa
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-04-19 14:01:34 +0200

    url

--- platform_nutella/platform_nutella/urls.py
+++ platform_nutella/platform_nutella/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 
 from food import views
 
